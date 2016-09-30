e421facf6f9192dd7fd054747959e331ea402982
michael dorfman <michael@michaels-MacBook-Air.local>
2016-09-29 21:51:46 -0400

    cleanup, rename

--- content.js
+++
@@ -1,7 +0,0 @@
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   chrome.tabs.executeScript({
-//     code: 'document.body.style.backgroundColor="red"'
-//   });
-// });
-
-alert("dog");
